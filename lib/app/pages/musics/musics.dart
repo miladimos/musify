a8a3c3427e1e99d92af62dbfe6ebf71cbf3ed20a
unknown <alireza.hajian80@gmail.com>
2022-01-28 18:25:10 +0330

    add music page

---
+++ lib/app/pages/musics/musics.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MusicsPage extends StatefulWidget {
+  const MusicsPage({Key? key}) : super(key: key);
+
+  @override
+  _MusicsPageState createState() => _MusicsPageState();
+}
+
+class _MusicsPageState extends State<MusicsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Music"),
+    );
+  }
+}
